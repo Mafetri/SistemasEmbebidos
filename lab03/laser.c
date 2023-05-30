69757c8b99054f2b978b9626e21d0c9ac2fe9caa
Manuel Triñanes <manutritri2@gmail.com>
2023-05-30 19:44:25 -0300

    Laser minor changes

--- lab03/laser.c
+++ lab03/laser.c
@@ -1,4 +1,6 @@
 #include "timer.h"
+#include "serial.h"
+#include "adc.h"
 #include <avr/interrupt.h>
 
 void send_char(char c, int port_number, volatile unsigned char* port) {
@@ -20,24 +22,26 @@ void send_char(char c, int port_number, volatile unsigned char* port) {
     *(port) ^= 1 << port_number; // Sets to high again
 }
 
-/*
-For the arduino receptionist:
-
-char receive_char(int port){
-    while(adc_get(port) > 700);
-    timer0_wait(50);
-
+#define NATURAL_LIGHT 3000
+// For the arduino receptionist:
     char received_char;
+char receive_char(int port){
+    // serial_put_int(adc_get(2), 4);
+    while(adc_get(port) > NATURAL_LIGHT);
+    timer0_wait(50);
+    timer0_wait(10);
+
+    received_char = 0;
     for(int i=0; i < 8; i++){
-        received_char |= (adc_get(port) > 700) ? (1 << i) : 0;
-        timer0_wait(100);
+        received_char |= (adc_get(port) > NATURAL_LIGHT) ? (1 << i) : (0 << i);
+        timer0_wait(50);
     }
 
-    while(adc_get(2) < 700);
+    while(adc_get(port) < NATURAL_LIGHT);
     
-    return receive_char;
+    return received_char;
 }
-*/
+
 
 int main(void) {
     volatile unsigned char * DDR_D = 0x2A;
@@ -48,10 +52,19 @@ int main(void) {
     *(DDR_D) = 1 << laser_port;
 
     timer0_init();
+    serial_init();
+    adc_init();
     sei();
-
+    char char_sent, char_received;
     while(1) {
-        timer0_wait(1000);
-        send_char('a', laser_port, DDR_D);
+        // Send
+        // caracter = serial_get_char();
+        // send_char(caracter, laser_port, DDR_D);
+
+        // Receive
+        char_received = receive_char(2);
+        serial_put_char(char_received);
+        serial_put_new_line();
+        
     }
 }
