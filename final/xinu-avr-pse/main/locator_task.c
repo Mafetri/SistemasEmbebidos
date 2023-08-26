14518eb3f4175830d9cfd7eb33e781a6490f764f
Manuel Triñanes <manutritri2@gmail.com>
2023-08-26 16:17:08 -0300

    Locator working properly

--- final/xinu-avr-pse/main/locator_task.c
+++ final/xinu-avr-pse/main/locator_task.c
@@ -1,8 +1,8 @@
 #include <xinu.h>
 #include <mat.h>
 
-#define WHEEL_DIAMETER 6
-#define WHEEL_SEPARATION 10
+#define WHEEL_DIAMETER 3
+#define WHEEL_SEPARATION 55.6
 #define ENCODER_RES 20
 #define PI 3.14159265359
 
@@ -26,17 +26,17 @@ int locator(void)
 	float right_distance = 0.0;
 	float center_distance = 0.0;
 
-	char output[24];
-	while (1) {
+	while (1)
+	{
 		// Read the wheel tick counts from last measurement
 		delta_left_ticks = left_wheel_ticks - last_left_ticks;
 		delta_right_ticks = right_wheel_ticks - last_right_ticks;
-        last_left_ticks = left_wheel_ticks;
-        last_right_ticks = right_wheel_ticks;
+		last_left_ticks = left_wheel_ticks;
+		last_right_ticks = right_wheel_ticks;
 
 		// Calculate wheel odometry
-        left_distance = (PI * WHEEL_DIAMETER / ENCODER_RES) * delta_left_ticks;
-        right_distance = (PI * WHEEL_DIAMETER / ENCODER_RES) * delta_right_ticks;
+		left_distance = (PI * WHEEL_DIAMETER / ENCODER_RES) * delta_left_ticks;
+		right_distance = (PI * WHEEL_DIAMETER / ENCODER_RES) * delta_right_ticks;
 		center_distance = (left_distance + right_distance) / 2.0;
 
 		// Update robot position and angle
@@ -44,6 +44,6 @@ int locator(void)
 		car_x += center_distance * cos(car_angle);
 		car_y += center_distance * sin(car_angle);
 
-        sleepms(100);
+		sleepms(100);
 	}
 }
